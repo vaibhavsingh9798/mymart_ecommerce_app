ba0bce98677f5aee6e78d20953ecb93b2090bcf0
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2024-01-12 11:00:14 +0530

    add logic to add a new product to the Firebase list

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Row } from "react-bootstrap"
+import { Button, Col, Container, Row ,Form} from "react-bootstrap"
 import './Product.css'
 import { useContext, useState, useEffect, useCallback } from "react"
 import CartContext from "../Store/Cart-auth"
@@ -7,20 +7,25 @@ const Product = () =>{
   const [products,setProducts] = useState([])
   const [loading,setLoading] = useState(true)
   const [error,setError] = useState(null)
-
-const fetchProduct= useCallback(async () => {
+  const [customProduct,setCustomProduct] = useState({title:'',price:0,image:''})
+  
+  const url = 'https://react-mymart-default-rtdb.firebaseio.com/'
+const fetchProduct = useCallback(async () => {
     try{
-  let response = await  fetch('https://fakestoreapi.com/produc')
+  let response = await  fetch(`${url}/products.json`)
    let data    = await response.json()
-     let transformedItems = data.map((product) => {
-         return {
-             id: product.id,
-             title: product.title.split(' ').splice(0,4).join(' '),
-             price:product.price ,
-             imageUrl: product.image
-         }
-     })
-     setProducts(transformedItems)
+   if(!data) throw new Error('Data Not Found !');
+   console.log('data...',data)
+    let items = []
+    for(let key in data){
+      let item = {
+        title: data[key].title,
+        price: data[key].price,
+        image: data[key].image
+      }
+       items.push(item)
+    }
+    setProducts(items)
     }catch(err){
       setError(err.message)
     }finally{
@@ -28,12 +33,14 @@ const fetchProduct= useCallback(async () => {
     }
    setLoading(false)
  },[])
+
       useEffect(()=>{
           fetchProduct()
       },[fetchProduct])
  
     const cartCtx = useContext(CartContext)
-    const handleAdd = (event,item)=>{
+
+    const handleAddToCart = (event,item)=>{
         event.preventDefault()
         cartCtx.addToCart(item)
        
@@ -46,10 +53,10 @@ const fetchProduct= useCallback(async () => {
                     <Col key={ind} sm={6} md={6} lg={6}   className="text-center mt-5" >
                     <div className="product-card">
                       <h5>{product.title}</h5>
-                      <img src={product.imageUrl} alt={product.title} style={{ width: '100px', height: '100px', objectFit: 'cover' }}/>
+                      <img src={product.image} alt={product.title} style={{ width: '100px', height: '100px' }}/>
                          <Row className=" mt-4">
                         <Col> <p>Rs {product.price}</p></Col>
-                        <Col><Button onClick={(event) => handleAdd(event,product)} className="w-10">Add to Cart</Button></Col>
+                        <Col><Button onClick={(event) => handleAddToCart(event,product)} className="w-10">Add to Cart</Button></Col>
                        </Row>
                     </div>
                     </Col>
@@ -58,15 +65,60 @@ const fetchProduct= useCallback(async () => {
              </Container>)
         }
 
+        const  handleSubmit = async (e) =>{
+               e.preventDefault()
+              console.log('hs',customProduct)
+              try{
+              const response = await fetch(`${url}/products.json`,{
+                method:"POST",
+                body:JSON.stringify(customProduct),
+                headers : {'Content-Type' : 'application/json'}
+              })
+
+              const data = await response.json()
+              console.log('response data ..',data)
+            }catch(err){
+              setError(err.message)
+            }
+           }
+    
+ 
+        const addProduct = () =>{
+            return (
+                <Container className="justify-content-md-center mt-4">
+                <Row className="justify-content-md-center">
+                     <h3 className="fw-bold text-center mt-4">Add Product</h3>
+                  <Col md={6}>
+                    <Form>
+                  
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" name='title' value={customProduct.title} onChange={(e) => setCustomProduct({...customProduct,[e.target.name]:e.target.value}) }/>
+                        <Form.Label>Price</Form.Label>
+                        <Form.Control type="number" name='price' value={customProduct.price} onChange={(e) => setCustomProduct({...customProduct,[e.target.name]:e.target.value})} />
+                        <Form.Label>Image URL</Form.Label>
+                        <Form.Control type="text" name='image' value={customProduct.image} onChange={(e) => setCustomProduct({...customProduct,[e.target.name]:e.target.value})} />
+                        <Row className="justify-content-center">
+                        <Col md={6}>
+                       <button onClick={handleSubmit} className="btn btn-primary">Submit</button>
+                       </Col>
+                       </Row>
+                    </Form>
+                  </Col>
+                </Row>
+              </Container>
+            )
+        }
+
+       
     return(
         <>
      
         {!loading && products.length > 0 && printProduct()}
         {!loading && error &&<h5>{error}</h5>}
           {loading && <h5>Loading.....</h5>}
-       
+          {addProduct()}
         </>
     )
 }
 
-export default Product;
+export default Product; 
